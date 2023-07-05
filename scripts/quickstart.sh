3b7e00143ae8babec6c40253c60fbd9b03b11a96
SenneDataroots <senne@dataroots.io>
2023-07-05 12:07:42 +0200

    Move scripts to separate folder

---
+++ scripts/quickstart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 -m venv contradiction_detection
+source contradiction_detection/bin/activate
+python3 -m pip install .
+source scripts/setup.sh
---
+++ scripts/setup_azure.sh
@@ -0,0 +1,10 @@
+export OPENAI_API_TYPE=azure
+export OPENAI_API_VERSION="2023-03-15-preview"
+# The deployment name of your Azure OpenAI embedding model.
+export DEPLOYMENT_EMBEDDING=
+# The deployment name of your Azure OpenAI large language model.
+export DEPLOYMENT_LLM=
+# The base URL for your Azure OpenAI resource.  You can find this in the Azure portal under your Azure OpenAI resource.
+export OPENAI_API_BASE=
+# The API key for your Azure OpenAI resource.  You can find this in the Azure portal under your Azure OpenAI resource.
+export OPENAI_API_KEY=
