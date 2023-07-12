62e892eed8eeac0caf0aa1a0c16c06e69609f099
SenneDataroots <senne@dataroots.io>
2023-07-12 11:31:37 +0200

    Fix failing assertions caused by conflict between log formatting and pre-commit-hook regarding trailing whitespaces

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -16,15 +16,21 @@ def test_format_retrieved_documents() -> None:
     """Test whether source documents are formatted correctly for logging."""
     formatted_docs = format_retrieved_documents([source_doc1, source_doc2])
 
-    expected = """
+    assert (
+        """
         1. Retrieved chunk from document Source 1 with the following content:
         Content of source doc 1.
+    """
+        in formatted_docs
+    )
 
+    assert (
+        """
         2. Retrieved chunk from document Source 2 with the following content:
         Content of source doc 2.
     """
-
-    assert expected in formatted_docs
+        in formatted_docs
+    )
 
 
 def test_log_contradiction_result_success(caplog: LogCaptureFixture) -> None:
@@ -41,7 +47,8 @@ def test_log_contradiction_result_success(caplog: LogCaptureFixture) -> None:
 
     log_contradiction_result(chunk, llm_result, contradiction=False)
 
-    no_contradiction_result = """
+    assert (
+        """
         No contradiction detected for document Test source.
 
         Conclusion from the LLM:
@@ -51,15 +58,9 @@ def test_log_contradiction_result_success(caplog: LogCaptureFixture) -> None:
         Content of a chunk.
 
         Retrieved documents:
-
-        1. Retrieved chunk from document Source 1 with the following content:
-        Content of source doc 1.
-
-        2. Retrieved chunk from document Source 2 with the following content:
-        Content of source doc 2.
     """
-
-    assert no_contradiction_result in caplog.text
+        in caplog.text
+    )
 
 
 def test_log_contradiction_result_failure(caplog: LogCaptureFixture) -> None:
@@ -80,26 +81,33 @@ def test_log_contradiction_result_failure(caplog: LogCaptureFixture) -> None:
 
     log_contradiction_result(chunk, llm_result, contradiction=True)
 
-    contradiction_result = """
+    # Assertions are split in three because otherwise the log formatting
+    # conflicts with the pre-commit hooks regarding trailing whitespaces
+
+    assert (
+        """
         Contradiction detected for document Test source.
 
         Conclusion from the LLM:
+    """
+        in caplog.text
+    )
 
+    assert (
+        """
 \t\t        INCONSISTENT: the following contradictions were detected for this chunk:
 \t\t        1. Contradiction1
 \t\t        2. Contradiction2
-\t\t
+\t\t"""
+        in caplog.text
+    )
 
+    assert (
+        """
         Content of new document:
         Content of a chunk.
 
         Retrieved documents:
-
-        1. Retrieved chunk from document Source 1 with the following content:
-        Content of source doc 1.
-
-        2. Retrieved chunk from document Source 2 with the following content:
-        Content of source doc 2.
     """
-
-    assert contradiction_result in caplog.text
+        in caplog.text
+    )
